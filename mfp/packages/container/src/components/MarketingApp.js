9ae1240ea54c937886e4fde3b05d3a923898ab9e
Theodor Virgolici <tvirg@softserveinc.com>
2023-12-07 13:53:12 +0200

    commit

--- mfp/packages/container/src/components/MarketingApp.js
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -1,12 +1,25 @@
 import { mount as mountMarketing } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const marketingAppReg = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mountMarketing(marketingAppReg.current);
-    });
+        const { onParentNavigate } = mountMarketing(marketingAppReg.current, {
+            onNavigate: ({ pathname: nextPathName }) => {
+                const { pathName } = history.location;
+
+                console.log(pathName);
+                if (pathName !== nextPathName) {
+                    history.push(nextPathName);
+                }
+            }
+        });
+
+        // history.listen(onParentNavigate);
+    }, []);
 
     return (
         <div ref={marketingAppReg}>
--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (element) => {
-    ReactDOM.render(<App />, element)
+//Mount fct to start app
+const mount = (element, { onNavigate, defaultHistroy }) => {
+    const history = defaultHistroy || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, element);
+
+    return {
+        onParentNavigate({ pathname: nextPathName }) {
+            const { pathName } = history.location;
+
+            if (pathName != nextPathName) {
+                history.push(nextPathName);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === "development") {
     const devRoot = document.getElementById('_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {
+            defaultHistroy: createBrowserHistory()
+        });
     }
 };
 
